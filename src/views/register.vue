775360e7883b5aee0e9394a453c5dd3aa2f9ba95
gtorres7 <gtorres6@ucol.mx>
2020-11-10 17:57:30 -0600

    Register view and api pettion

--- src/views/register.vue
+++ src/views/register.vue
@@ -1,8 +1,73 @@
 <template>
-    <h1>this is register</h1>
+  <v-container>
+    <v-card class="mx-auto my-12" width="500px">
+      <v-card-title>
+        <h1 class="display-1">Registrar</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form ref="form" v-on:submit.prevent="register">
+
+            <v-text-field
+            label="Nombre"
+            v-model="name"
+            :rules="[inputRules.required]"
+          />
+
+          <v-text-field
+            label="Correo"
+            v-model="email"
+            :rules="[inputRules.required]"
+          />
+
+          <v-text-field
+            label="Contraseña"
+            v-model="password"
+            :rules="[inputRules.required]"
+            type="password"
+          />
+
+          <v-card-actions>
+            <v-btn type="submit" color="info">registrar</v-btn>
+            <v-spacer />
+          </v-card-actions>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-export default {
-};
+  import axios from 'axios'
+
+  export default {
+    data: () => ({
+      name:'',  
+      email: '',
+      password: '', 
+      inputRules: {
+        required: value => value===0 || !!value || 'campo obligatorio',
+      }
+    }),
+
+    methods: {
+      async register() {
+        if(!this.$refs.form.validate()) return
+        
+        const {email, password, name} = this
+        try {
+            const {data} = await axios.post(
+                '/users/register',
+                {name, email, password}
+            )
+
+            this.$store.dispatch('reciveToken', data.token)
+            this.$router.push('/welcome');
+        }
+        catch (e) {
+          console.error(e)
+        }
+      }
+    },
+  }
 </script>
+

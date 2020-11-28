f859eed286950c212808494c4fdc4324e3989799
gtorres7 <gtorres6@ucol.mx>
2020-11-28 17:33:19 -0600

    exported empoints to public

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import VueRouter from 'vue-router'
 import Login from '../views/login.vue'
 import Register from '../views/register.vue'
 import Welcome from '../views/welcome.vue'
+import Scan from '../views/scan.vue'
+import Session from '../views/session.vue'
 
 Vue.use(VueRouter)
 
@@ -29,7 +31,17 @@ const routes = [
     path: '/welcome',
     name: 'Welcome',
     component: Welcome
-  }
+  },
+  {
+    path: '/scan',
+    name: 'Scan',
+    component: Scan
+  },
+  {
+    path: '/new-session',
+    name: 'NewSession',
+    component: Session
+  },
 ]
 
 const router = new VueRouter({

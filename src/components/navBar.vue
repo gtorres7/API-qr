d8d38f2418d81a84edca5395505f69a56a95a30c
gtorres7 <gtorres6@ucol.mx>
2020-11-28 18:33:11 -0600

    vue icon redirects to home

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -12,15 +12,7 @@
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
           transition="scale-transition"
           width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+          @click="goHome"
         />
       </div>
 
@@ -53,6 +45,9 @@
       logout() {
         store.dispatch('removeToken')
         this.$router.push('/login');
+      },
+      goHome() {
+        this.$router.push('/')
       }
     }
   }
